2dbc1c19363eaac97db3ee51e3348f15dfec23a8
Virat Agnihotri <viratagnihotri178@gmail.com>
2026-06-20 13:00:31 +0530

    login/signup route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import SideBar from "./components/SideBar"
 import NavBar from "./components/NavBar"
 import Trash from "./components/trash"
+import LandingPage from "./components/LandingPage";
+import Login from "./components/Login"
+import SignIn from "./components/SignUp"
 function App() {
   const [count, setCount] = useState(0)
   const message="hello";
-
+  const [Active,SetActive]=useState("")
   return (
-   <div>
-    <SideBar/>
-    <NavBar/>
-    {/* <Trash abcd={message}/> */}
-   </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<LandingPage />}/>
+    <Route path="/login" element={<Login />}/>
+    <Route path="signup" element={<SignIn />}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
