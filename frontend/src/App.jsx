2dcaecc03f0775deda5b778e1b24b481000546ce
AmanRajSharma001 <amanrajsharma699@gmail.com>
2026-06-20 12:24:54 +0530

    updating frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import SideBar from "./components/SideBar"
+import Login from "./components/Login"
+import SignUp from "./components/SignUp"
 function App() {
   const [count, setCount] = useState(0)
   const message="hello";
 
   return (
    <div>
-    <SideBar abcd={message} />
+    <Login />
    </div>
   )
 }
